aeda3f56fdc2a563fb88c162daef4a4157c298d1
arfisogo <59739665+arfisogo@users.noreply.github.com>
2020-11-15 22:30:40 -0300

    Create publisher.java

---
+++ publisher.java
@@ -0,0 +1,49 @@
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+import java.lang.Math;
+
+public class MqttPublishSample {
+
+    public static void main(String[] args) {
+        String topic        = "Publicar uma temperatura a cada segundo";
+        int qos             = 2;
+        String broker       = "tcp://mqtt.eclipse.org:1883";
+        String clientId     = "JavaSample";
+        MemoryPersistence persistence = new MemoryPersistence();
+        int maxima             = 45;
+        int minima             = 15;
+
+        try {
+            MqttClient sampleClient = new MqttClient(broker, clientId, persistence);
+            MqttConnectOptions connOpts = new MqttConnectOptions();
+            connOpts.setCleanSession(true);
+            System.out.println("Connecting to broker: "+broker);
+            sampleClient.connect(connOpts);
+            System.out.println("Connected");
+
+            while (true)
+            {
+                int RandomTemperature = ((Math.random() * (maxima - minima)) + minima);
+                System.out.println("A próxima temperatura é: " + RandomTemperature);
+                MqttMessage message = new MqttMessage(RandomTemperature.getBytes());
+                message.setQos(qos);
+                sampleClient.publish(topic, message);
+                System.out.println("Mensagem publicada");
+                Thread.sleep(1000);
+            }
+            sampleClient.disconnect();
+            System.out.println("Disconnected");
+            System.exit(0);
+        } catch(MqttException me) {
+            System.out.println("reason "+me.getReasonCode());
+            System.out.println("msg "+me.getMessage());
+            System.out.println("loc "+me.getLocalizedMessage());
+            System.out.println("cause "+me.getCause());
+            System.out.println("excep "+me);
+            me.printStackTrace();
+        }
+    }
+}
